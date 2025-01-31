b07249143871f21a2e86aadc5fa0d7666ac947a4
JADER124 <jaderlopez186@gmail.com>
2025-01-30 23:07:43 -0500

    Config API REST django

---
+++ TaskFlow_API/TaskFlow/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from rest_framework import routers
+from .views import UserView
+
+router = routers.DefaultRouter()
+router.register(r'users',UserView,'user')
+urlpatterns = [
+    path('usuarios',include(router.urls))
+]
