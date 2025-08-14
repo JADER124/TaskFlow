79212eac2cececfc3dde45468cb2ad847306f889
juangonzalez09 <gonzalezjuanm58@gmail.com>
2025-08-13 20:10:23 -0500

    FIX: Se arregla linea en setcookie para correfir el bug de inicio de sesion despues de vencer la cookie

--- TaskFlow_API/TaskFlow/views/setcookie.py
+++ TaskFlow_API/TaskFlow/views/setcookie.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.contrib.auth import get_user_model
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes,authentication_classes
 from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework_simplejwt.exceptions import TokenError
@@ -11,6 +11,7 @@ User = get_user_model()
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
+@authentication_classes([]) 
 def setcookie(request):
     access = request.data.get('access')
     refresh = request.data.get('refresh')
